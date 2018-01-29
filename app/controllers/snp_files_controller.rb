90d2f0d443ee76a19eda5507b825eece4b925051
Ricardo H. Ramirez-Gonzalez <ricardo.ramirez-gonzalez@jic.ac.uk>
2018-01-29 19:38:33 +0000

    Reduced verbosity

--- app/controllers/snp_files_controller.rb
+++ app/controllers/snp_files_controller.rb
@@ -23,11 +23,11 @@ class SnpFilesController < ApplicationController
      @snp_file.reference = reference.name
      parsed_file = helpers.parse_file(@snp_file, form_snp_file[:polymarker_input], reference)
      
-     puts "___Aabout to save____"
+     #puts "___Aabout to save____"
     if @snp_file.save!
-      puts @snp_file.inspect
-
-      redirect_to snp_file_path(@snp_file), notice: "The snp_file has been created!" and return
+      #puts @snp_file.inspect
+      
+      redirect_to snp_file_path(@snp_file), notice: "New snp file" and return
     end
     render 'new'
   end
