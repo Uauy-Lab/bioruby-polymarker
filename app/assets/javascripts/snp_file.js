c2d2bc7cb31679a34219648ffd68b1ff619dcf73
Ricardo H. Ramirez-Gonzalez <ricardo.ramirez-gonzalez@jic.ac.uk>
2018-02-09 23:19:26 +0100

    Added on clikc functionality

--- app/assets/javascripts/snp_file.js
+++ app/assets/javascripts/snp_file.js
@@ -1,27 +1,22 @@
-load_primers_table = function (snp_file_id, div, done) {
-
+load_primers_table = function (snp_file_id, div, done, local_msa) {
 	var grid_div = $("#" + div);
 	var primers_url =  snp_file_id  +'/input.json';
-	
 	var general = [
-		{name: 'ID', caption:'ID', width: '100px'},
-		{name: 'Chr', caption:'Chr', width: '40px'}
+		{name: 'ID', title:'ID', width: '100px'},
+		{name: 'Chr', title:'Chr', width: '40px'}
 	]
-
 	var polymarker_output = [
-	{name: 'total_contigs', caption:'Matches', width: '20px'},
-	{name: 'SNP_type', caption:'Type', width: '65px'},
-	{name: 'A', caption:'A', width: '150px'},
-	{name: 'B', caption:'B', width: '150px'},
-	{name: 'common', caption:'Common', width: '150px'},
-	{name: 'primer_type', caption:'Primer type', width: '80px'},
-	{name: 'errors', caption:'Errors', width: '205px'},
+	{name: 'total_contigs', title:'Aln', width: '30px'},
+	{name: 'SNP_type', title:'Type', width: '65px'},
+	{name: 'A', title:'A', width: '150px'},
+	{name: 'B', title:'B', width: '150px'},
+	{name: 'common', title:'Common', width: '150px'},
+	{name: 'primer_type', title:'Primer type', width: '80px'},
+	{name: 'errors', title:'Errors', width: '205px'},
 	]
-
 	var sequence = [
-		{name: 'Sequence', caption:'Sequence', width: '820px'}
+		{name: 'Sequence', title:'Sequence', width: '810px'}
 	]
-
 	if(done){
 		general = general.concat(polymarker_output);
 	}else{
@@ -30,25 +25,20 @@ load_primers_table = function (snp_file_id, div, done) {
 	
 	columns_array = general
 	grid_div.jsGrid({
-		filtering: true,
+		filtering: false,
         editing: false,
         sorting: true,
         paging: true,
         autoload: true,
-		
-		//url: primers_url,
+		pageSize: 5,
 		fields: columns_array,
 		controller: {
             loadData: function() {
                 var d = $.Deferred();
- 				console.log("Loading?");
                 $.ajax({
                     url: primers_url,
                     dataType: "json"
                 }).done(function(response) {
-                	console.log("Respones");
-                	console.log(response.records);
-                	console.log(d.resolve);
                     d.resolve(response.records);
                 });
  
@@ -56,23 +46,58 @@ load_primers_table = function (snp_file_id, div, done) {
             }
         },
 		rowClick: function(event) {
-			console.log(event);
+			load_mask(snp_file_id, event.item, local_msa);
 		}
 	});
-
-	
-	
 };
 
+find_target_sequence = function(item, seqs){
+	var target = item["Chr"];
+	console.log("Target: " + target);
+	var chr_index = 0;
+	var i = 0;
+	current_best = 0;
+	seqs.map( function(seq) {
+		var split = seq.name.split("-");
+		var name_tmp = split[split.length - 1];
+		console.log(split)
+		console.log(name_tmp);
+		console.log(split.length);
+		if(split.length > 1) {
+			var split_2 = name_tmp.split("_")
+			console.log(split_2);
+			var name = split_2[0];
+			var score = parseFloat(split_2[2])
+			console.log(name);
+			console.log(score);
 
-load_mask = function(snp_file_id, marker, local_msa ){
+			if(name== target && score > current_best){
+				chr_index = i;
+				current_best = score;
+				console.log("The best!")
+			}
+		}
+		i++;
+	});
+	return chr_index;
+}
+
+
+
+load_mask = function(snp_file_id, item, local_msa ){
+	var marker = item["ID"];
 	var fasta_url = snp_file_id + "/" + marker + ".fasta" ;
 	var seqs = fasta.read(fasta_url);
+	console.log(local_msa.seqs)
+	
+	local_msa.seqs.reset();
 	seqs.then(function(result) {
 		console.log(result);
+		find_target_sequence(item, result);
 		local_msa.seqs.add(result);
 		local_msa.render();
 	});
+
 }
 
 setup_msa_div = function (div) {
