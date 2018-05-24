cc0473193c4ad86374ebe2433c8133de3eb43370
Bijan <bijan.ghasemiafshar@gmail.com>
2018-05-24 14:55:27 +0100

    Configurations for setting up

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,21 +1,5 @@
-const { environment } = require('@rails/webpacker')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const webpack = require('webpack')
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
-// Add an ProvidePlugin
-environment.plugins.set('Provide',  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    jquery: 'jquery',
-    w2ui: 'w2ui',
-  })
-)
-
-const config = environment.toWebpackConfig()
-
-config.resolve.alias = {
-  jquery: "jquery/src/jquery",
-  w2ui: "w2ui/w2ui",
-}
+const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
