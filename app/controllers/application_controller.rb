50abc32e44bbe9ce153f794a68b30177a82ce532
Ricardo H. Ramírez-Gonzalez <homonecloco@users.noreply.github.com>
2018-11-19 15:14:58 +0000

    Update application_controller.rb
    Really fixed the indentation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def get_example  	
-
   	reference = Reference.find_by(name: params[:ref])
-
   	example = {"value" => reference.example.chomp!}  	  	
-
   	respond_to do |format|
       format.json { render json: example}
     end
- 	end 	
+  end
 end
