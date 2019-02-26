90e1d1b2e6bd325deb66e9762dadab622dcff9a0
Ricardo H. Ramirez-Gonzalez <ricardo.ramirez-gonzalez@jic.ac.uk>
2019-02-26 17:28:30 +0000

    Style edits. Added limit of 10 markers.

--- app/controllers/snp_files_controller.rb
+++ app/controllers/snp_files_controller.rb
@@ -7,12 +7,10 @@ class SnpFilesController < ApplicationController
 
   def new
     @snp_file = SnpFile.new
-    #puts @snp_file.inspect
     @snp_file
   end
 
   def create
-    
     begin      
       @snp_file = SnpFile.new
       form_snp_file = snp_file_params
@@ -20,26 +18,20 @@ class SnpFilesController < ApplicationController
       @snp_file.filename = form_snp_file[:polymarker_input].original_filename unless form_snp_file[:polymarker_input].nil?
       @snp_file.email_hash = Digest::MD5.hexdigest @snp_file.email
       @snp_file.status = "New"
-
       reference = Reference.find_by(name: form_snp_file[:reference])
-
       @snp_file.reference = reference.name
-
-
       unless form_snp_file[:polymarker_input].nil?
         parsed_file = helpers.parse_file(@snp_file, form_snp_file[:polymarker_input], reference)
       else
         parsed_file = helpers.parse_manual_input(@snp_file, params[:polymarker_manual_input][:post], reference)
       end      
-      
       throw "0 SNPs found" if @snp_file.snps.size == 0
+      throw "More than 10 SNPs found." if @snp_file.snps.size > 10
       if @snp_file.save!        
         PolyMarkerWorker.perform_async(@snp_file.id, request.base_url)
         redirect_to snp_file_path(@snp_file), notice: "SNP file uploaded successfully" and return
       end
-
       render 'new'
-
     rescue => e    
       flash[:error] = "Please attach a CSV file with the correct data format\n #{e.to_s} " 
       session[:return_to] ||= request.referer
@@ -59,7 +51,6 @@ class SnpFilesController < ApplicationController
   end
 
   def show
-
     @scheduled_number = 0
     @snp_file = SnpFile.find params["id"]
     if @snp_file.status != "New"
@@ -70,13 +61,10 @@ class SnpFilesController < ApplicationController
       helpers.store_job_in_local_queue(@snp_file.id.to_s) if queue.size > 0
       @scheduled_number = helpers.get_job_queue_index @snp_file.id.to_s
     end
-
     @is_done = ((@snp_file.status.include? "ERROR") || (@snp_file.status.include? "DONE"))
-
   end
 
   def array_to_json(records_array, fields)
-
     records=Array.new
     records_array.each do |record|
       record_h = Hash.new
@@ -103,10 +91,8 @@ class SnpFilesController < ApplicationController
       polymarker_input = snp_file.polymarker_output[id]
       polymarker_input["primer_type"] = polymarker_input["primer_type"].sub("chromosome_","") if polymarker_input["primer_type"]
       polymarker_input["SNP_type"] = polymarker_input["SNP_type"].sub("homoeologous","hom") if polymarker_input["SNP_type"]
-
       new_records << e.merge(polymarker_input)
     end
-    new_records.first.keys.each { |e| puts "{field: '#{e}', caption:'#{e}', size: '50px'}, \n" }
     markers["records"] = new_records
     markers
   end
@@ -114,7 +100,6 @@ class SnpFilesController < ApplicationController
   def show_input
     @snp_file = SnpFile.find params["id"]    
      records = get_snps_and_markers(@snp_file)
-     #records["value"] = records["records"]
      respond_to do |format|
        format.html
  			 format.json {
@@ -128,46 +113,30 @@ class SnpFilesController < ApplicationController
   end
 
   def get_mask_file
-
     snp_file = SnpFile.find params["id"]    
-
     fasta = snp_file.mask_fasta
     fasta_data=''
-        
-
     fasta.each do |key, value|
       fasta_data += value  
     end    
-
     send_data fasta_data, filename: "exons_genes_and_contigs.fa"
-    
   end
 
 
   def get_primers
-
     snp_file = SnpFile.find params["id"]
-
     primer_data = snp_file.polymarker_output    
     primer_headers = primer_data.values[0].keys.join(",")
     primer_values = ''        
-
     primer_output = CSV.generate do |csv|
       csv << [primer_headers]
     end
-
     primer_data.each do |key, value|      
       primer_values = value.values.join(",")      
-
       primer_output += CSV.generate do |csv|
         csv << [primer_values]
       end
-
     end    
-
     send_data primer_output, filename: "Primers.csv"
-    
   end
-
-
 end
--- app/views/markdown/_home.md
+++ app/views/markdown/_home.md
@@ -4,3 +4,5 @@ Request primers
 PolyMarker is an automated bioinformatics pipeline for SNP assay development which increases the probability of generating homoeologue-specific assays for polyploid species. PolyMarker generates a multiple alignment between the target SNP sequence and the selected reference genome (from the drop off menu in green below). It then generates a mask with informative polymorphic positions between homoeologs which are highlighted with respect to the target genome.
 
 See [About](/about) to know how to prepare your input. For wheat, we have [designed primers](/designed_primers) for the iSelect 90K chip and the Axiom 820k chip.
+
+**Please only submit 10 primers per request**
