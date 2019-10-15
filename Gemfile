9d094bf8187021504cb329f63c34d936e5ed4fd7
homonecloco <ricardo.ramirez-gonzalez@jic.ac.uk>
2019-10-15 09:59:42 +0200

    Added a function to convert the data drame into something that we can use with chart.js

--- Gemfile
+++ Gemfile
@@ -76,4 +76,6 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
+#For the plots
+
 gem "daru", "~> 0.2.2"
--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,7 +1,33 @@
 class StatusController < ApplicationController
+  
+
   def summary
+  	summ = ReferenceHelper.summary_by_month
+
+  	@plots = Hash.new
+
+  	@plots["requests"] = df_to_plot(summ, :count_requests)
+
+
+
+
   end
 
   def load
+  end
+
+  private
+  def df_to_plot(df, column)
+  	groups = df.group_by([:reference])
+  	ret = Hash.new
+  	groups.each_group do |dfg|
+  		ref = dfg[:reference].first
+  		months = Hash.new
+  		dfg.each_row do |row|
+  			months[row[:month]] = row[column]
+  		end
+  		ret[ref] = months
+  	end
+  	ret
   end
 end
